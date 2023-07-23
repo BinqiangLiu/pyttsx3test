29521741d6dc6de246ed8b5d96de57a805dc25f5
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-07-23 13:29:42 +0800

    Create pyttsx3test.py

---
+++ pyttsx3test.py
@@ -0,0 +1,34 @@
+#使用这个录音模块：https://pypi.org/project/audio-recorder-streamlit/
+import streamlit as st
+import openai
+import pyttsx3
+import numpy as np
+
+# Load environment variables
+from dotenv import load_dotenv
+import os
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Function to convert text to speech using pyttsx3
+def text_to_speech(text):
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 150)
+    engine.setProperty("voice", "english-us")
+    engine.save_to_file(text, "response.mp3")
+    engine.runAndWait()
+    with open("response.mp3", "rb") as file:
+        response_audio = file.read()
+#    os.remove("response.mp3")  # Remove the temporary audio file
+    return response_audio
+
+# Main function to run the Streamlit app
+def main():
+    st.title("Audio to Chat App")
+    text = "中华人民共和国"
+        # Audio output section
+    st.header("Step 2: Listen to the AI Response")
+    st.audio(text_to_speech(text), format="audio/mp3", start_time=0)
+
+if __name__ == "__main__":
+    main()
